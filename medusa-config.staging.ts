d1e2999e8499f5a20edeb825a3c3ce0bd3103d97
danrfletcher <danrfletcher@outlook.com>
2024-12-14 19:02:02 +0000

    Add configs for each environment

--- medusa-config.staging.ts
+++ medusa-config.staging.ts
@@ -1,4 +1,4 @@
-//production configuration
+//staging configuration - admin credentials: admin@medusajs.com supersecret
 import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 
 loadEnv(process.env.NODE_ENV || "development", process.cwd());
@@ -6,14 +6,8 @@ loadEnv(process.env.NODE_ENV || "development", process.cwd());
 module.exports = defineConfig({
   admin: {
     disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
-    backendUrl: process.env.MEDUSA_BACKEND_URL,
   },
   projectConfig: {
-    workerMode: process.env.MEDUSA_WORKER_MODE as
-      | "shared"
-      | "worker"
-      | "server",
-    redisUrl: process.env.REDIS_URL,
     databaseUrl: process.env.DATABASE_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
@@ -41,26 +35,6 @@ module.exports = defineConfig({
             },
           },
         ],
-      },
-    },
-    {
-      resolve: "@medusajs/medusa/workflow-engine-redis",
-      options: {
-        redis: {
-          url: process.env.REDIS_URL,
-        },
-      },
-    },
-    {
-      resolve: "@medusajs/medusa/event-bus-redis",
-      options: {
-        redisUrl: process.env.REDIS_URL,
-      },
-    },
-    {
-      resolve: "@medusajs/medusa/cache-redis",
-      options: {
-        redisUrl: process.env.REDIS_URL,
       },
     },
   ],
