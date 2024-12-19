b257b49df153273ffcebb9ea748efad7d361f052
danrfletcher <danrfletcher@outlook.com>
2024-12-19 22:42:50 +0000

    Export type of competition module

--- src/modules/competitions/index.ts
+++ src/modules/competitions/index.ts
@@ -3,6 +3,8 @@ import CompetitionModuleService from "./services/competition-service";
 
 export const COMPETITION_MODULE = "competitions";
 
+export type { CompetitionModuleService } 
+
 export default Module(COMPETITION_MODULE, {
     service: CompetitionModuleService,
 });
