e00ddea560ea3fb52a8bb82a91d43a5848482d71
danrfletcher <danrfletcher@outlook.com>
2024-12-19 22:47:12 +0000

    Fix migrations to reflect unique properties

--- src/modules/competitions/migrations/Migration20241219224610.ts
+++ src/modules/competitions/migrations/Migration20241219224610.ts
@@ -1,12 +1,14 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20241219224106 extends Migration {
+export class Migration20241219224610 extends Migration {
 
   async up(): Promise<void> {
     this.addSql('create table if not exists "geoguesser_guess" ("id" text not null, "order_item_id" text not null, "map_coordinates" text[] not null, "created_at" timestamptz not null default now(), "updated_at" timestamptz not null default now(), "deleted_at" timestamptz null, constraint "geoguesser_guess_pkey" primary key ("id"));');
+    this.addSql('CREATE UNIQUE INDEX IF NOT EXISTS "IDX_geoguesser_guess_order_item_id_unique" ON "geoguesser_guess" (order_item_id) WHERE deleted_at IS NULL;');
     this.addSql('CREATE INDEX IF NOT EXISTS "IDX_geoguesser_guess_deleted_at" ON "geoguesser_guess" (deleted_at) WHERE deleted_at IS NULL;');
 
     this.addSql('create table if not exists "geoguesser_image" ("id" text not null, "image_id" text not null, "product_variant_id" text not null, "map_coordinates" text not null, "created_at" timestamptz not null default now(), "updated_at" timestamptz not null default now(), "deleted_at" timestamptz null, constraint "geoguesser_image_pkey" primary key ("id"));');
+    this.addSql('CREATE UNIQUE INDEX IF NOT EXISTS "IDX_geoguesser_image_product_variant_id_unique" ON "geoguesser_image" (product_variant_id) WHERE deleted_at IS NULL;');
     this.addSql('CREATE INDEX IF NOT EXISTS "IDX_geoguesser_image_deleted_at" ON "geoguesser_image" (deleted_at) WHERE deleted_at IS NULL;');
   }
 
