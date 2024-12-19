387e5f158b1c31d5ede9be023f13e52a3780a27b
danrfletcher <danrfletcher@outlook.com>
2024-12-19 20:30:38 +0000

    Remove redundant links (for now)

--- src/links/geoguesser-image-image.ts
+++
@@ -1,9 +0,0 @@
-import { defineLink } from "@medusajs/utils";
-import CompetitionModule from "../modules/competitions";
-import ProductModule from "@medusajs/medusa/product";
-
-export default defineLink(ProductModule.linkable.product, {
-  linkable: CompetitionModule.linkable.geoguesserImage,
-  isList: true,
-  deleteCascade: true,
-});
--- src/links/geoguesser-image-product-variant.ts
+++
@@ -1,8 +0,0 @@
-import { defineLink } from "@medusajs/utils";
-import CompetitionModule from "../modules/competitions";
-import ProductModule from "@medusajs/medusa/product";
-
-export default defineLink(ProductModule.linkable.productVariant, {
-  linkable: CompetitionModule.linkable.geoguesserImage,
-  deleteCascade: true,
-});
