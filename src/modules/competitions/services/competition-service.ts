e43a2c69af1378bd356df2d695721f23c6b74318
danrfletcher <danrfletcher@outlook.com>
2024-12-19 22:44:19 +0000

    Remove redundant type

--- src/modules/competitions/services/competition-service.ts
+++ src/modules/competitions/services/competition-service.ts
@@ -2,7 +2,6 @@ import { MedusaService } from "@medusajs/framework/utils";
 import { DAL } from "@medusajs/framework/types";
 import GeoguesserImage from "../models/geoguesser-image";
 import GeoguesserGuess from "../models/geoguesser-guess";
-import { GeoguesserImageType } from "../types";
 
 type InjectedDependencies = {
   baseRepository: DAL.RepositoryService;
--- src/modules/competitions/types/index.ts
+++
@@ -1,6 +0,0 @@
-import { InferTypeOf } from "@medusajs/framework/types";
-import GeoguesserImageId from "../models/geoguesser-image";
-
-export type ProductVariantGeoguesserImageId = InferTypeOf<
-  typeof GeoguesserImageId
->;
