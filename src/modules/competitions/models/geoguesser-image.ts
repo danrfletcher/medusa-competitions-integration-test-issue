ff615fe2b1df8494e270844bf4ac925d1882035e
danrfletcher <danrfletcher@outlook.com>
2024-12-19 22:42:11 +0000

    Update data model for geoguesser image

--- src/modules/competitions/models/geoguesser-image.ts
+++ src/modules/competitions/models/geoguesser-image.ts
@@ -1,11 +1,10 @@
 import { model } from "@medusajs/framework/utils";
 
 const GeoguesserImage = model.define("geoguesser_image", {
-  id: model.id().primaryKey(),
+  id: model.id({ prefix: "geogimg" }).primaryKey(),
   image_id: model.text(),
-  product_id: model.text(),
-  product_variant_ids: model.array().nullable(),
-  map_coordinates: model.text()
+  product_variant_id: model.text(),
+  map_coordinates: model.text(),
 });
 
 export default GeoguesserImage;
