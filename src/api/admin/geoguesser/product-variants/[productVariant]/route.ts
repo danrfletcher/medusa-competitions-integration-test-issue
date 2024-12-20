9a2f5a43e4cda586e4fb54dc6cc3ac5e7799a54f
danrfletcher <danrfletcher@outlook.com>
2024-12-20 18:27:28 +0000

    Completed POST API route for geoguesser on product Varients

--- src/api/admin/geoguesser/product-variants/[productVariant]/route.ts
+++ src/api/admin/geoguesser/product-variants/[productVariant]/route.ts
@@ -35,3 +35,34 @@ export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
     });
   }
 };
+
+// export const PATCH = async (req: MedusaRequest, res: MedusaResponse) => {
+//   const competitionService: CompetitionModuleService =
+//     req.scope.resolve(COMPETITION_MODULE);
+
+//   const data = CreateGeoguesserImagesSchema.parse(req.body);
+//   const { productVariant } = req.params;
+//   const {  }
+
+//   try {
+//     const updateGeoguesserImg = await competitionService.updateGeoguesserImages([],{
+//       ...data,
+//       product_variant_id: productVariant,
+//     });
+
+//     res.json({
+//       geoguesser_image: updateGeoguesserImg,
+//     });
+//   } catch (error) {
+//     if (error instanceof z.ZodError) {
+//       return res.status(400).json({
+//         message: "Validation failed",
+//         errors: error.flatten().fieldErrors,
+//       });
+//     }
+
+//     res.status(400).json({
+//       message: error.message,
+//     });
+//   }
+// };
