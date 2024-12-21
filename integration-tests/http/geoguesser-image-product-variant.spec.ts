bee78076de1927976aaafd89d10f064a1b6d7c90
danrfletcher <danrfletcher@outlook.com>
2024-12-21 18:30:58 +0000

    update name to reflect route

--- integration-tests/http/geoguesser-image-product-variant.spec.ts
+++ integration-tests/http/geoguesser-image-product-variant.spec.ts
@@ -1,25 +1,23 @@
 import { medusaIntegrationTestRunner } from "@medusajs/test-utils";
-import seedFunction from "../../src/scripts/test-data";
-import { ContainerRegistrationKeys } from "@medusajs/framework/utils";
+import seedTestData from "src/scripts/seed-test-data";
 import {
   COMPETITION_MODULE,
   CompetitionModuleService,
 } from "src/modules/competitions";
 
 medusaIntegrationTestRunner({
+  dbName: process.env.DB_NAME,
   testSuite: ({ api, getContainer }) => {
     let query;
-    let context;
     let geoguesserService: CompetitionModuleService;
 
     //seed before all tests
     beforeAll(async () => {
-      await seedFunction({
+      await seedTestData({
         container: getContainer(),
         args: [],
       });
 
-      query = getContainer().resolve(ContainerRegistrationKeys.QUERY);
       geoguesserService = getContainer().resolve(COMPETITION_MODULE);
       geoguesserService;
     });
@@ -29,37 +27,17 @@ medusaIntegrationTestRunner({
       await geoguesserService.deleteGeoguesserImages(["*"]);
     });
 
-    describe("/admin/geoguesser/:productVariant", () => {
-      describe("POST /admin/geoguesser/:productVariant", () => {
-        let productVariantId;
-        let productVariant;
-        let id;
-        let product_id;
-        let product;
-        let images;
-        let image;
-        let imageId;
-        let authenticated;
+    describe("/admin/geoguesser/image/:productVariant", () => {
+      let productVariantId;
+      let productVariant;
+      let product_id;
+      let image;
+      let imageId;
+      let authenticated;
 
-        //get product variants, images & authenticate the user before all tests
-        beforeAll(async () => {
-          const { data: variants } = await query.graph({
-            entity: "product_variant",
-            fields: ["*"],
-          });
-          productVariant = variants[0];
-          ({ id: productVariantId, product_id } = productVariant);
-
-          const { data: product } = await query.graph({
-            entity: "product",
-            fields: ["images.*"],
-            filters: {
-              id: product_id,
-            },
-          });
-          image = product[0].images[0];
-          ({ id: imageId } = image);
-
+      //get product variants, images & authenticate the user before all tests
+      beforeAll(async () => {
+        try {
           const loginResponse = await api.post(`/auth/user/emailpass`, {
             email: "admin@test.com",
             password: "admin123",
@@ -70,31 +48,50 @@ medusaIntegrationTestRunner({
               Authorization: `Bearer ${token}`,
             },
           };
-        });
 
-        it("200: Returns correct geoguesser_image object for a valid product variant, image & map coordinates", async () => {
-          const requestBody = {
-            image_id: imageId,
-            map_coordinates: "55.41565370130057, -1.7079462411391673",
-          };
-
-          const response = await api.post(
-            `/admin/geoguesser/product-variants/${productVariantId}`,
-            requestBody,
+          const productResponse = await api.get(
+            `/admin/products`,
             authenticated
           );
 
-          expect(response.status).toEqual(200);
-          expect(response.data).toHaveProperty("geoguesser_image");
-          expect(response.data.geoguesser_image).toMatchObject({
-            id: expect.stringMatching(/^geogimg_/),
-            image_id: imageId,
-            product_variant_id: productVariantId,
-            map_coordinates: requestBody.map_coordinates,
-            created_at: expect.any(String),
-            updated_at: expect.any(String),
-            deleted_at: null,
-          });
+          productVariant = productResponse.data.products[0].variants[0];
+          ({ id: productVariantId, product_id } = productVariant);
+
+          image = productResponse.data.products[0].images[0];
+          ({ id: imageId } = image);
+        } catch (error) {
+          console.log(error.response.status, error.response.data);
+        }
+      });
+      describe("POST /admin/geoguesser/image/:productVariant", () => {
+        it("200: Returns correct geoguesser_image object for a valid product variant, image & map coordinates", async () => {
+          try {
+            const requestBody = {
+              image_id: imageId,
+              map_coordinates: "55.41565370130057, -1.7079462411391673",
+            };
+
+            const test = await api.get(`/admin/products`, authenticated);
+            console.log("⚡ ~ test:", test.data);
+
+            const response = await api.post(
+              `/admin/geoguesser/image/${productVariantId}`,
+              requestBody,
+              authenticated
+            );
+
+            expect(response.status).toEqual(200);
+            expect(response.data).toHaveProperty("geoguesser_image");
+            expect(response.data.geoguesser_image).toMatchObject({
+              id: expect.stringMatching(/^geogimg_/),
+              image_id: imageId,
+              product_variant_id: productVariantId,
+              map_coordinates: requestBody.map_coordinates,
+              created_at: expect.any(String),
+              updated_at: expect.any(String),
+              deleted_at: null,
+            });
+          } catch (error) {}
         });
 
         it("400: Returns bad request for malformed map coordinates", async () => {
@@ -105,7 +102,7 @@ medusaIntegrationTestRunner({
             };
 
             const response = await api.post(
-              `/admin/geoguesser/product-variants/${productVariantId}`,
+              `/admin/geoguesser/image/${productVariantId}`,
               requestBody,
               authenticated
             );
@@ -125,7 +122,7 @@ medusaIntegrationTestRunner({
             };
 
             const response = await api.post(
-              `/admin/geoguesser/product-variants/${productVariantId}`,
+              `/admin/geoguesser/image/${productVariantId}`,
               requestBody,
               authenticated
             );
@@ -143,7 +140,7 @@ medusaIntegrationTestRunner({
             };
 
             const response = await api.post(
-              `/admin/geoguesser/product-variants/${productVariantId}`,
+              `/admin/geoguesser/image/${productVariantId}`,
               requestBody
               // No authentication header
             );
@@ -160,7 +157,7 @@ medusaIntegrationTestRunner({
             };
 
             const response = await api.post(
-              `/admin/geoguesser/product-variants/invalid_variant_id`,
+              `/admin/geoguesser/image/invalid_variant_id`,
               requestBody,
               authenticated
             );
@@ -178,10 +175,12 @@ medusaIntegrationTestRunner({
               image_id: "invalid_image_id",
               map_coordinates: "55.41565370130057, -1.7079462411391673",
             };
-            console.log("⚡ ~ productVariantId:", productVariantId);
+
+            const test = await api.get(`/admin/products`, authenticated);
+            console.log("⚡ ~ test:", test.data);
 
             const response = await api.post(
-              `/admin/geoguesser/product-variants/${productVariantId}`,
+              `/admin/geoguesser/image/${productVariantId}`,
               requestBody,
               authenticated
             );
@@ -190,6 +189,12 @@ medusaIntegrationTestRunner({
             expect(error.response.data.message).toEqual("Image not found");
           }
         });
+      });
+      describe("GET /admin/geoguesser/image/:productVariant", () => {
+        it("200: Returns correct geoguesser_image object for a given product variant", async () => {});
+        it("401: Returns unauthorized for a missing authentication header", () => {});
+        it("404: Returns not found for an invalid product variant", async () => {});
+        it("404: Returns geoguesser image not found for a valid product variant not associated with a geoguesser image", async () => {});
       });
     });
   },
